b8779a706edcb7cbe7a719428d3e8e52516a7a86
rka28 <karim.bara@epitech.eu>
2024-04-22 09:37:20 +0200

    feat:ajout de image des model de voiturre ajout d'un carousel

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,13 +1,32 @@
 
 import '../menu/menu.css';
-import menu1 from '../Assets/menu-mc.png';
+
 import menu2 from '../Assets/menu1.png';
 import menu3 from '../Assets/menu2.png';
+import menu4 from '../Assets/menu4.png';
+import menu5 from '../Assets/menu5.png';
+import menu6 from '../Assets/menu6.png';
+import menu7 from '../Assets/menu7.png';
+import menu8 from '../Assets/menu8.png';
+import menu9 from '../Assets/menu9.png';
+import menu10 from '../Assets/menu10.png';
+import menu11 from '../Assets/menu11.png';
+import menu12 from '../Assets/menu12.png';
+import menu13 from '../Assets/menu13.png';
+import menu14 from '../Assets/menu14.png';
+import menu15 from '../Assets/menu15.png';
+import menu16 from '../Assets/menu16.png';
+import menu17 from '../Assets/menu17.png';
+import menu18 from '../Assets/menu18.png';
+import menu19 from '../Assets/menu19.png';
+import menu20 from '../Assets/menu20.png';
+import menu21 from '../Assets/menu21.png';
+import menu22 from '../Assets/menu22.png';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import { useState } from 'react'
-import Carousel from 'react-bootstrap/Carousel';
-// import { Carousel } from 'react-responsive-carousel';
+// import Carousel from 'react-bootstrap/Carousel';
+import { Carousel } from 'react-responsive-carousel';
 // import ExampleCarouselImage from 'components/ExampleCarouselImage';
 
 
@@ -30,32 +49,21 @@ function Menu() {
 				<div className='container-menu '>
 					<div className='contanair-border'>
 						<div className='container-fluid'>
-							<div className='rent'>
+							
 								<h5>Mercedes GLE</h5>
-								<Carousel activeIndex={index} onSelect={handleSelect}>
+								<Carousel>
 
-									<Carousel.Item>
-										{/* <ExampleCarouselImage text="Second slide" /> */}
-										<Carousel.Caption>
-										
-										</Carousel.Caption>
-									</Carousel.Item>
-									<Carousel.Item>
-										<img src={menu1} alt=""  />
-										{/* <ExampleCarouselImage text="Third slide" /> */}
-										<img src={menu1} alt="" width={300} />
-										<Carousel.Caption>
-										
-										</Carousel.Caption>
-									</Carousel.Item>
+									<img src={menu2} alt=""  />
+									<img src={menu3} alt=""  />
 								</Carousel>
+						
 								<h6>Classe SUV</h6>
 								<div className='btn-menu'>
 									<button className='btn-rent'> 120€/2h</button>
 									<button className='btn-rent'>En savoir plus</button>
 
 								</div>
-							</div>
+							
 
 						</div>
 					</div>
@@ -65,9 +73,11 @@ function Menu() {
 						<div className='container-fluid'>
 							<div className='rent'>
 								<h5>Mercedes GLE</h5>
-								
-									<img src={menu2} alt="" width={300} />
-									<img src={menu3} alt="" width={300} />
+								<Carousel>
+
+									<img src={menu4} alt=""  />
+									<img src={menu5} alt=""  />
+								</Carousel>
 								
 								<h6>Classe SUV</h6>
 								<div className='btn-menu'>
@@ -90,8 +100,8 @@ function Menu() {
 							<div className='rent'>
 								<h5>Mercedes GLE</h5>
 								<Carousel>
-									<img src={menu1} alt="" width={300} />
-									<img src={menu1} alt="" width={300} />
+									<img src={menu7} alt="" width={300} />
+									<img src={menu6} alt="" width={300} />
 								</Carousel>
 								<h6>Classe SUV</h6>
 								<div className='btn-menu'>
@@ -110,52 +120,8 @@ function Menu() {
 							<div className='rent'>
 								<h5>Mercedes GLE</h5>
 								<Carousel>
-									<img src={menu1} alt="" width={300} />
-									<img src={menu1} alt="" width={300} />
-								</Carousel>
-								<h6>Classe SUV</h6>
-								<div className='btn-menu'>
-									<button className='btn-rent'> 120€/2h</button>
-									<button className='btn-rent'>En savoir plus</button>
-
-								</div>
-							</div>
-
-						</div>
-					</div>
-				</div>
-
-			</div>
-			<div className='flex-menu'>
-
-				<div className='container-menu '>
-					<div className='contanair-border'>
-						<div className='container-fluid'>
-							<div className='rent'>
-								<h5>Mercedes GLE</h5>
-								<Carousel>
-									<img src={menu1} alt="" width={300} />
-									<img src={menu1} alt="" width={300} />
-								</Carousel>
-								<h6>Classe SUV</h6>
-								<div className='btn-menu'>
-									<button className='btn-rent'> 120€/2h</button>
-									<button className='btn-rent'>En savoir plus</button>
-
-								</div>
-							</div>
-
-						</div>
-					</div>
-				</div>
-				<div className='container-menu '>
-					<div className='contanair-border'>
-						<div className='container-fluid'>
-							<div className='rent'>
-								<h5>Mercedes GLE</h5>
-								<Carousel>
-									<img src={menu1} alt="" width={300} />
-									<img src={menu1} alt="" width={300} />
+									<img src={menu8} alt="" width={300} />
+									<img src={menu9} alt="" width={300} />
 								</Carousel>
 								<h6>Classe SUV</h6>
 								<div className='btn-menu'>
@@ -178,8 +144,8 @@ function Menu() {
 							<div className='rent'>
 								<h5>Mercedes GLE</h5>
 								<Carousel>
-									<img src={menu1} alt="" width={300} />
-									<img src={menu1} alt="" width={300} />
+									<img src={menu10} alt="" width={300} />
+									<img src={menu11} alt="" width={300} />
 								</Carousel>
 								<h6>Classe SUV</h6>
 								<div className='btn-menu'>
@@ -198,8 +164,8 @@ function Menu() {
 							<div className='rent'>
 								<h5>Mercedes GLE</h5>
 								<Carousel>
-									<img src={menu1} alt="" width={300} />
-									<img src={menu1} alt="" width={300} />
+									<img src={menu13} alt="" width={300} />
+									<img src={menu12} alt="" width={300} />
 								</Carousel>
 								<h6>Classe SUV</h6>
 								<div className='btn-menu'>
@@ -222,8 +188,53 @@ function Menu() {
 							<div className='rent'>
 								<h5>Mercedes GLE</h5>
 								<Carousel>
-									<img src={menu1} alt="" width={300} />
-									<img src={menu1} alt="" width={300} />
+									<img src={menu14} alt="" width={300} />
+									<img src={menu15} alt="" width={300} />
+								</Carousel>
+								<h6>Classe SUV</h6>
+								<div className='btn-menu'>
+									<button className='btn-rent'> 120€/2h</button>
+									<button className='btn-rent'>En savoir plus</button>
+
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				<div className='container-menu '>
+					<div className='contanair-border'>
+						<div className='container-fluid'>
+							<div className='rent'>
+								<h5>Mercedes GLE</h5>
+								<Carousel>
+									<img src={menu17} alt="" width={300} />
+									<img src={menu16} alt="" width={300} />
+								</Carousel>
+								<h6>Classe SUV</h6>
+								<div className='btn-menu'>
+									<button className='btn-rent'> 120€/2h</button>
+									<button className='btn-rent'>En savoir plus</button>
+
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<div className='flex-menu'>
+
+				<div className='container-menu '>
+					<div className='contanair-border'>
+						<div className='container-fluid'>
+							<div className='rent'>
+								<h5>Mercedes GLE</h5>
+								<Carousel>
+									<img src={menu18} alt="" width={300} />
+									<img src={menu19} alt="" width={300} />
+									<img src={menu22} alt="" width={300} />
 								</Carousel>
 								<h6>Classe SUV</h6>
 								<div className='btn-menu'>
@@ -242,8 +253,8 @@ function Menu() {
 							<div className='rent'>
 								<h5>Mercedes GLE</h5>
 								<Carousel>
-									<img src={menu1} alt="" width={300} />
-									<img src={menu1} alt="" width={300} />
+									<img src={menu20} alt="" width={300} />
+									<img src={menu21} alt="" width={300} />
 								</Carousel>
 								<h6>Classe SUV</h6>
 								<div className='btn-menu'>
